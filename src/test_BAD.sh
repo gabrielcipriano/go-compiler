3e1a6dd4abfbd29da65820c769ea42264093a631
Gabriel Cipriano <gabrielfcipriano@gmail.com>
2022-11-24 11:33:47 -0300

    fix test script

--- src/test_BAD.sh
+++ src/test_BAD.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ROOT=/home/ryan/Documents/2022-2/Compiladores/go-compiler
+ROOT=/Users/cipriano/git/go-compiler
 ANTLR_PATH=$ROOT/tools/antlr-4.11.1-complete.jar
 CLASS_PATH_OPTION="-cp .:$ANTLR_PATH"
 GRAMMAR_NAME=GO
@@ -14,5 +14,8 @@ for infile in `ls $IN/*.go`; do
   outfile=$OUT/${base/.go/.out}
   echo Running $base
   # /usr/bin/env /usr/lib/jvm/java-11-openjdk-amd64/bin/java @/tmp/cp_1a318z5kum1cicb8dttgo494v.argfile Main $infile > $outfile
-  make run FILE="$infile" 2> $outfile
+  cat $infile > $outfile
+  echo -e '\n\n________________________________________________________________________________\n\n' >> $outfile
+  make run FILE="$infile" 2>> $outfile
+
 done
